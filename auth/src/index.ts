8f7c97e5fd4f95336745c0d708d0f26b2a903e4e
stephenprin <stephenprince427@gmail.com>
2023-03-30 08:48:37 +0100

    ingress controller and skaffold config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';  
-
+import { Request, Response } from 'express';
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req:Request, res:Response) => {
+    res.send("hello there")
+})
 
 app.listen(3000, () => { 
-    console.log('Server started on port 3000!');
+    console.log('Server started on port 3000!!!');
 });
