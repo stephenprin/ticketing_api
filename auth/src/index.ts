b7c756d53e00bb85e33f984a804a92462b0f280d
stephenprin <stephenprince427@gmail.com>
2023-04-11 15:13:40 +0100

    authenication added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,11 +9,19 @@ import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 import 'express-async-errors';
+import cookieSession from 'cookie-session';
 
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
+
+app.use(cookieSession({
+    signed: false,
+    secure:true
+}))
+
 
 
 
@@ -21,15 +29,19 @@ app.use(currentUserRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 app.use(signinRouter);
+app.use(errorHandler)
 
 
 app.all('*', async(req, res, next) => {
     new NotFoundError()
 })
 
-app.use(errorHandler)
+
 
 const start = async () => { 
+    if (!process.env.JWT_KEY) { 
+        throw new Error('JWT_KEY must be defined');
+    }
     try {
         await mongoose.connect(process.env.DATABASE_URL!);
         console.log('Connected to MongoDB!!!');
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,11 +1,14 @@
 import express, {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator';
+import { BadRequestError } from '../errors/bad-requestError';
 import { RequestValidationError } from '../errors/request-validation';
 
 import { User } from '../models/user-model';
+import jwt from 'jsonwebtoken';
+
 const router = express.Router();
 
-router.post('api/users/signup', [
+router.post('/api/users/signup', [
     body('email')
         .isEmail()
         .withMessage('Email must be valid'),
@@ -15,7 +18,7 @@ router.post('api/users/signup', [
         .withMessage('Password must be between 4 and 20 characters')
 ],
    async (req: Request, res: Response) => { 
-   
+ 
         const error = validationResult(req)
         if (!error.isEmpty()) {
             throw new RequestValidationError(error.array())
@@ -25,14 +28,27 @@ router.post('api/users/signup', [
        
        const existingUser = await User.findOne({ email })
        if (existingUser) { 
-           console.log('Email in use')
-            return res.send({})
+            throw new BadRequestError('Email in use')
        }
 
        const user = User.build({ email, password })
        await user.save()
 
-      res.status(201).send(user)
+       //gENERATE JWT 
+       const userJwt = jwt.sign({
+           id: user.id,
+              email: user.email
+       }, process.env.JWT_KEY!)
+
+       //store it on session object
+
+       req.session = {
+           jwt: userJwt
+           
+       }
+
+
+       res.status(201).send(user)
     })
 
 export { router as signupRouter }; 
